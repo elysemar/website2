7065061aec865956bd57607e63e69953c1510f6d
elysemar <142545977+elysemar@users.noreply.github.com>
2024-03-22 17:14:30 +0000

    Fri, Mar 22, 2024, 10:14 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,75 +4,86 @@ closeBtn = document.getElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
-score = 0
-
-brickRowCount = 9
-brickColumnCount = 5
-
+//create ball properties
 ball = {
     x: canvas.width / 2,
-    y: canvas.height / 2,
+    y : canvas.height / 2,
     size: 10,
     speed: 4,
     dx: 4,
     dy: -4,
 }
 
+//create paddle properties
 paddle = {
     x: canvas.width / 2 - 40,
-    y: canvas.height - 20,
+    y: canvas.height -20,
     w: 80,
     h: 10,
     speed: 8,
     dx: 0,
+
 }
 
+// create brick properties
 brickInfo = {
     w: 70,
     h: 20,
     padding: 10,
     offsetX: 45,
     offsetY: 60,
-    visible: true
+    visible: true,
+
 }
 
+
+
+score = 0
+brickRowCount = 9
+brickColumnCount = 5
+
+// create bricks
 bricks = []
-for(let i = 0; i < brickRowCount; i++) {
+for(let i = 0; i <brickRowCount; i++) {
     bricks[i] = []
-    for (let j = 0; j < brickColumnCount; j++){
+    for(let j = 0; j < brickColumnCount; j++) {
         const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
         const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
         bricks[i][j] = {x, y, ...brickInfo}
     }
 }
 
+// draw ball on canvas
 function drawBall () {
-    ctx.beginPath()
-    ctx.arc(ball.x, ball.y, ball.size, 0,  Math.PI * 2);
-    ctx.fillStyle = 'rgb(182, 208, 226)'
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true); // Outer circle
+    ctx.fillStyle = 'rgb(255, 89, 148)';
     ctx.fill()
     ctx.closePath()
 }
 
+// draw paddle on canvas
 function drawPaddle () {
-    ctx.beginPath()
+    ctx.beginPath();
     ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
-    ctx.fillStyle =  'rgb(182, 208, 226)'
+    ctx.fillStyle = 'rgb(255, 89, 148)';
     ctx.fill()
     ctx.closePath()
 }
 
+// draw score on canvas
 function drawScore() {
-    ctx.font='20px Arial'
+    ctx.font ='20px Arial'
     ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
 }
 
+// draw bricks on canvas
 function drawBricks() {
     bricks.forEach(column => {
         column.forEach(brick => {
             ctx.beginPath()
             ctx.rect(brick.x, brick.y, brick.w, brick.h)
-            ctx.fillStyle = brick.visible ?  'rgb(182, 208, 226)' : 'transparent';
+            ctx.fillStyle = brick.visible ? 'rgb(255, 89, 148)' : 'transparent';
             ctx.fill()
             ctx.closePath()
         })
@@ -81,57 +92,72 @@ function drawBricks() {
 
 console.log(bricks)
 
+// draw everything
 function draw() {
-    drawPaddle()
+    ctx.clearRect(0,0,canvas.width,canvas.height)
     drawBall()
+    drawPaddle()
     drawScore()
     drawBricks()
 }
 
-//keydown Event
-function keyDown(e){
-    //console.log(e.key)
-    if (e.key == 'ArrowRight' || e.key == 'left') {
-        paddle.dx = -paddle.speed
-    }
-}
-
-function keyDown(e){
-    //console.log(e.key)
-    if (e.key == 'ArrowRight' || e.key == 'Right' || e.key ==) {
-        paddle.dx = -paddle.speed
-    }
-}
-
-function draw() {
-
-}
-
-//move paddle on canvas
-function movePaddle () {
+// move paddle on canvas
+function movePaddle() {
     paddle.x = paddle.x + paddle.dx
+
+    // wall detection
+    if (paddle.x < 0) {
+        paddle.x = 0
+    }
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w
+    }
 }
 
-//keyboard event handlers
+
+// keydown event
+function keyDown(e) {
+    // console.log(e.key)
+    if (e.key == 'ArrowRight' || e.key == 'Right') {
+        paddle.dx = paddle.speed
+    }
+    if (e.key == 'ArrowLeft' || e.key == 'Left') {
+        paddle.dx = -paddle.speed
+    }
+}
+// keyUp event
+function keyUp(e) {
+    if (e.key == 'ArrowRight' ||
+    e.key == 'Right' ||
+    e.key == 'ArrowLeft' ||
+    e.key == 'Left') {
+        paddle.dx = 0
+    }
+
+
+}
+
+// keyboard event handlers
 document.addEventListener('keydown', keyDown)
-document.addEventListener('keyup', keyUp)
+document.addEventListener('keyUp', keyUp)
 
 
-
-function update () {
-
+// update the canvas drawing and animation
+function update() {
+    movePaddle()
     draw()
     requestAnimationFrame(update)
 }
 
 update()
 
-
-
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
 })
 
+
 closeBtn.addEventListener('click', () => {
     rules.classList.remove('show')
 })
+
+
