c62477bc937790b98f0537e5e6fc4c43539af4bb
elysemar <142545977+elysemar@users.noreply.github.com>
2024-04-02 16:52:20 +0000

    Tue, Apr 2, 2024, 9:52 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -139,9 +139,10 @@ function moveBall() {
                     ball.x - ball.size > brick.x && //left
                     ball.x + ball.size < brick.x + brick.w //right
                     ball.y - ball.size < brick.y + brick.h //bottom
-                ) 
+                ) {
                 ball.dy = -1 * ball.dy
                 brick.visible = false
+                }
             }
         })
     })
