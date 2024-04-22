ee1579b6d337b34840f1c40ceab90564877d2d39
elysemar <142545977+elysemar@users.noreply.github.com>
2024-04-22 16:49:55 +0000

    Mon, Apr 22, 2024, 9:49 AM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,13 @@
+const wordEl = document.getElementById('word')
+wrongLettersEl = document.getElementById('wrong-letters')
+const plaAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard]
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = words[selectedIndex]
+
